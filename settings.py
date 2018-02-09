57fd23234aad665f60b15b622c90a20b2dca9abc
Mrinal <waliamrinal@gmail.com>
2018-02-10 02:46:34 +0530

    add this setting file for alien invasion game

---
+++ settings.py
@@ -0,0 +1,12 @@
+class Settings():
+	
+	def __init__(self):
+		'''initialize the game settings'''
+		#screem resolution
+		
+		self.screen_width = 1366
+		self.screen_height = 728
+		
+		#background color
+		
+		self.bg_color = (230,230,230)
