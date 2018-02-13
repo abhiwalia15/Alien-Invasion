c2ff1863c95492f8f406cf6d480f5ced93e9e6d9
Mrinal <waliamrinal@gmail.com>
2018-02-14 03:12:59 +0530

    add it

--- game_function.py
+++ game_function.py
@@ -3,27 +3,37 @@ import sys
 
 import pygame
 
+def check_keydown_events(event,ship):
+	'''respond to keypress.'''
+	#when a KEYDOWN event is detected we need to check whether the key pressed is one that triggers a certain event.
+	if event.key == pygame.K_RIGHT:
+	#move the ship towards the right.
+		ship.moving_right = True
+		
+	elif event.key == pygame.K_LEFT:
+		ship.moving_left = True
+		
+def check_keyup_events(event,ship):
+	'''respond to key releases.'''
+	if event.key == pygame.K_RIGHT:
+		ship.moving_right = False
+				
+	elif event.key == pygame.K_LEFT:
+		ship.moving_left = False
+
 def check_events(ship):
+	
 	#watch for mouse and keyboard events.
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				sys.exit()
 	
 			elif event.type == pygame.KEYDOWN:
-				#when a KEYDOWN event is detected we need to check whether the key pressed is one that triggers a certain event.
-				if event.key == pygame.K_RIGHT:
-					#move the ship towards the right.
-					ship.moving_right = True
+				check_keydown_events(event,ship)
 				
-				elif event.key == pygame.K_LEFT:
-					ship.moving_left = True
 					
 			elif event.type == pygame.KEYUP:
-				if event.key == pygame.K_RIGHT:
-					ship.moving_right = False
-				
-				elif event.key == pygame.K_LEFT:
-					ship.moving_left = False
+				check_keyup_events(event,ship)
 						
 def update_screen(ai_settings,screen,ship):
 	'''update the images on the screen and flip to the new screen,'''
