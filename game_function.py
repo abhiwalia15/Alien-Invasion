54b54163072692c25fcce0ecfb1a7a8e6361e669
Mrinal <waliamrinal@gmail.com>
2018-02-19 02:01:57 +0530

    commiting

--- game_function.py
+++ game_function.py
@@ -17,8 +17,9 @@ def check_keydown_events(event,ai_settings,screen,ship,bullets):
 	
 	elif event.key == pygame.K_SPACE:
 		#create a new bullet and add it to the bullets group.
-		new_bullet = Bullet(ai_settings,screen,ship)
-		bullets.add(new_bullet)
+		if len(bullets) < ai_settings.bullets_allowed:
+			new_bullet = Bullet(ai_settings,screen,ship)
+			bullets.add(new_bullet)
 		
 def check_keyup_events(event,ship):
 	'''respond to key releases.'''
--- settings.py
+++ settings.py
@@ -19,5 +19,7 @@ class Settings():
 		self.bullet_width = 3
 		self.bullet_height = 15
 		self.bullet_color = (60,60,60)
+		#limit the number of bullets that ship fires at once.
+		self.bullets_allowed = 3
 		
  
