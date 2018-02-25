4d91f77282df117724ad207a3d560bec6b423999
Mrinal <waliamrinal@gmail.com>
2018-02-25 16:01:01 +0530

    add

--- settings.py
+++ settings.py
@@ -4,18 +4,18 @@ class Settings():
 		'''initialize the game settings'''
 		#screem resolution
 		
-		self.screen_width = 1366
-		self.screen_height = 728
+		self.screen_width = 1200
+		self.screen_height = 800
 		
 		#background color
 		
 		self.bg_color = (230,230,230)
 
 		#ship speed settings
-		self.ship_speed_factor = 2.5
+		self.ship_speed_factor = 2
 		
 		#settings for bullets,these will create bullets of dark grey color and will travel up untill it disappears.
-		self.bullet_speed_factor = 2
+		self.bullet_speed_factor = 3
 		self.bullet_width = 3
 		self.bullet_height = 15
 		self.bullet_color = (60,60,60)
