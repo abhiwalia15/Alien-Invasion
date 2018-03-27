c1865c15c6b568a37bfba1c3e6d16168f12382a9
Mrinal <waliamrinal@gmail.com>
2018-03-27 23:16:30 +0530

    add these cahnges to this file

--- scoreboard.py
+++ scoreboard.py
@@ -18,3 +18,18 @@ class Scoreboard():
 		#prepare the initial scoring image.
 		self.prep_score()
 		
+	def prep_score(self):
+		'''turn the score into a rendered image'''
+		
+		score_str = str(self.stats.score)
+		self.score_image = self.font.render(score_str,true,self.text_color,self.ai_settings.bg_color)
+		
+		#Display the score at the top right of the screen.
+		self.score_rect = self.score_image.get_rect()
+		self.score_rect.right = self.screen_rect.right - 20
+		self.score_rect.top = 20
+		
+	def show_score(self):
+		'''Draw score to the screen'''
+		self.screen.blit(self.score_image,self.score_rect)
+		
