2ad20d6305b8f1ec2ff7062d437938ad779f3781
Mrinal <waliamrinal@gmail.com>
2018-03-29 20:47:17 +0530

    add these files

--- game_functions.py
+++ game_functions.py
@@ -112,9 +112,12 @@ def check_bullet_alien_collisions(ai_settings, screen, stats, sb, ship, aliens, 
             check_high_score(stats,sb)
     
     if len(aliens) == 0:
-        # Destroy existing bullets, speed up game and create new fleet.
+        #  If the entire fleet is destroyed, start a new level.
         bullets.empty()
         ai_settings.increase_speed()
+        #increase the level
+        stats.level += 1
+        sb.prep_level()
         create_fleet(ai_settings, screen, ship, aliens)
         
 def check_high_score(stats,sb):
--- scoreboard.py
+++ scoreboard.py
@@ -33,9 +33,10 @@ class Scoreboard():
 		self.score_rect.top = 20
 		
 	def show_score(self):
-		'''Draw score to the screen'''
+		'''Draw score and ships to the screen'''
 		self.screen.blit(self.score_image,self.score_rect)
 		self.screen.blit(self.high_score_image,self.high_score_rect)
+		self.screen.blit(self.level_image,self.level_rect)
 		
 	def prep_high_score(self):
 		'''turn the high score into a rendered image.'''
@@ -48,8 +49,16 @@ class Scoreboard():
 		self.high_score_rect.centerx = self.screen_rect.centerx
 		self.high_score_rect.top = self.score_rect.top
 		
-
-
+	def prep_level(self):
+		"""Turn the level into a rendered image"""
+		sefl.level_image = self.font.render(str(self.stats.level),True,self.text_color,self.ai_settings.bg_color)
+		
+		#position the level below the score.
+		self.level_rect = self.level_image.get_rect()
+		self.level_rect.right = self.score_rect.right
+		self.level_rect.top = self.score_rect.bottom + 10
+		
+	
 
 
 
