72fbceba0ae54245d6d7fcdf0019c73ab7c92211
Mrinal <waliamrinal@gmail.com>
2018-10-17 00:42:25 +0530

    changes made to settings file

--- settings.py
+++ settings.py
@@ -4,8 +4,8 @@ class Settings():
 		'''initialize the game's static settings'''
 		#screem resolution
 		
-		self.screen_width = 1400
-		self.screen_height = 700
+		self.screen_width = 1366
+		self.screen_height =728
 		
 		#background color
 		
